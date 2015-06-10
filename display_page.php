408cb32a878705b44215efe7c44a5b9c3ec02502
Chris F Ravenscroft <chris@voilaweb.com>
2015-06-09 21:14:12 -0400

    Fixed markdown pages rendering

--- display_page.php
+++ display_page.php
@@ -6,9 +6,40 @@
 $template->view('page');
 
 $slug = $db->escapeString($_GET['page']);
-$row = $db->querySingle("SELECT titles,content,status FROM mae_pages left join mae_pages_richtextpage on id=page_ptr_id where slug='" . $slug . "'", true);
+$row = $db->querySingle("SELECT titles,content,status,format_type FROM mae_pages left join mae_pages_richtextpage on id=page_ptr_id where slug='" . $slug . "'", true);
 if(empty($row)) {
     die("Page Ooops. 404 and all that :(");
+}
+
+if($row['format_type'] == 2) {
+    require 'thirdparty/Parsedown.php';
+    $Parsedown = new Parsedown();
+    $content = $Parsedown->text($row['content']);
+    $content2 = <<<EOB
+<script>
+jQuery(document).ready(function() {
+    jQuery('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
+});
+</script>
+EOB;
+}
+else if($row['format_type'] == 1) {
+    $content = $row['content'];
+    $content2 = <<<EOB
+<script>
+jQuery(document).ready(function() {
+    jQuery('pre').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
+});
+</script>
+EOB;
+}
+else {
+    $content = $row['content'];
+    $content2 = '';
 }
 
 if($user->can_edit) {
@@ -21,7 +52,7 @@ if($user->can_edit) {
     );
 }
 
-$content = str_replace('/refdirect/?obj=', '/refdirect/', $row['content']);
+$content = str_replace('/refdirect/?obj=', '/refdirect/', $content);
 
 $template->set('slug', $slug);
 $template->set('status', $row['status']);
